659737c95cc3f27cdbcf64438032b22df3149d1b
Neelesh <130912265+neeleshkr22@users.noreply.github.com>
2025-04-17 23:20:57 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,13 @@ import { PrismaClient } from "../generated/prisma";
 
 const prisma  =  new PrismaClient();
 
-prisma.user
+async function insertUser(username:string,password:string, firstName:string, lastName:string){
+    const res = await prisma.user.create({
+        data:{
+            email: username,
+            password,
+            firstName,
+            lastName
+        }
+    })
+}
